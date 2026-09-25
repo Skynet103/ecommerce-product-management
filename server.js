b610852265e41d39b33a6a589c45549814af98cd
Aayush Sinha <sinhaaayush357@gmail.com>
2026-09-25 15:35:23 +0530

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
 });
