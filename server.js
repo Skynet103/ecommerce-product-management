ba962b0bf5df2b0589331ede3a07225c3b1c2513
Aayush Sinha <sinhaaayush357@gmail.com>
2026-09-25 17:13:51 +0530

    Fix backend CORS

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const orderRoutes = require("./routes/orderRoutes");
 const connectDB = require("./config/db");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 connectDB();
 
